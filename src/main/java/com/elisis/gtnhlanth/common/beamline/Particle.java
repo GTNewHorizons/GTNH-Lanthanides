ff6ffea4a7c821f162d947f796e0c5976f13596f
unknown <gtandemmodding@gmail.com>
2022-11-20 20:32:40 +1100

    Add 'acceleratable' quality, localisation of particle naming

--- src/main/java/com/elisis/gtnhlanth/common/beamline/Particle.java
+++ src/main/java/com/elisis/gtnhlanth/common/beamline/Particle.java
@@ -1,16 +1,36 @@
 package com.elisis.gtnhlanth.common.beamline;
 
+import net.minecraft.util.StatCollector;
+
 public enum Particle {
-    ELECTRON(0, 0.511f),
-    PHOTON(1, 0);
+    ELECTRON(true, 0, 0.511f, "electron", "e\u207B"),
+    PHOTON(false, 1, 0, "photon", "\u03B3");
 
+	private boolean canAcc;
+	
     private float restMass;
+    
+    private String name;
+    private String shortName;
 
-    private Particle(int id, float restMass) { // ID is symbolic only
-        this.restMass = restMass;
+    private Particle(boolean canAcc, int id, float restMass, String name, String shortName) { // ID is symbolic only
+        this.canAcc = canAcc;
+    	this.restMass = restMass;
+    	this.name = name;
+    	this.shortName = shortName;
     }
 
     public float getMass() {
         return this.restMass;
     }
+    
+    public boolean canAccelerate() {
+    	return this.canAcc;
+    }
+    
+    public String getLocalisedName() {
+    	return StatCollector.translateToLocal("particle." + this.name) + " (" + this.shortName + ")";
+    }
+    
+    
 }
