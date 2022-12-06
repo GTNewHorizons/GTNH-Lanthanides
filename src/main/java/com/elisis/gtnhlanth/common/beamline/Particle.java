3162888b79ea23f9d1c5f8422105f6d720b86835
unknown <gtandemmodding@gmail.com>
2022-12-06 13:32:12 +1100

    Add more particles, specify units of mass, energy

--- src/main/java/com/elisis/gtnhlanth/common/beamline/Particle.java
+++ src/main/java/com/elisis/gtnhlanth/common/beamline/Particle.java
@@ -4,13 +4,16 @@ import net.minecraft.util.StatCollector;
 
 public enum Particle {
     ELECTRON(true, 0, 0.511f, 5000, "electron", "e\u207B"),
-    PHOTON(false, 1, 0, 0, "photon", "\u03B3");
+    PHOTON(false, 1, 0, 0, "photon", "\u03B3"),
+	NEUTRON(false, 2, 939.57f, 15000, "neutron", "n\u2070"),
+	PROTON(true, 3, 938.27f, 15000, "proton", "p\u207A"),
+	ALPHA(true, 4, 3727.38f, 8000, "alpha", "\u03B1");
 
     private boolean canAcc;
 
-    private float restMass;
+    private float restMass; //in MeV
     
-    private float maxSourceEnergy;
+    private float maxSourceEnergy; //in keV
 
     private String name;
     private String shortName;
